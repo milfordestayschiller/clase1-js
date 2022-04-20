6c05523fc274f35db7b292519b43fda3da22028d
milfordestayschiller <103213100+milfordestayschiller@users.noreply.github.com>
2022-04-20 15:34:56 -0400

    Ejercicios Asincronicos
    Ejercicios Asincronicos

---
+++ Ejercicios/ejercicio1.js
@@ -0,0 +1,3 @@
+var edad = prompt("Cual es tu edad?")
+var dias = edad * 365;
+document.write("Usted tiene una edad de  " +edad+ " años , la cual indico anteriormente, lo cual equivale a: " +dias+ " dias")
---
+++ Ejercicios/ejercicio2.js
@@ -0,0 +1,13 @@
+var miEdad = 36;
+var miEdadMaxima = 90;
+var snack = "Snickers";
+var snacksPorDia = 2;
+
+var restoDeVida = miEdadMaxima - miEdad;
+
+var SnacksPorRestoDeVida = (restoDeVida) * snacksPorDia
+
+var valorSnak = 300;
+
+var precioSnacksRestodeVida = SnacksPorRestoDeVida * valorSnak
+alert("Necesitaras " +SnacksPorRestoDeVida+ " snacks Para comer por el resto de tu vida y gastaras " +precioSnacksRestodeVida+ " pesos por los snacks por el resto de tu vida")
